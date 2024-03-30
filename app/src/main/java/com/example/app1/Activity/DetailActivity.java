1e9c676926847275c52db8de067e6f7c9982757e
nguyentung <nguyenxuantung003@gmail.com>
2024-03-30 20:05:35 +0700

    Xuan Tung commit layout detail

---
+++ app/src/main/java/com/example/app1/Activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.app1.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app1.R;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
